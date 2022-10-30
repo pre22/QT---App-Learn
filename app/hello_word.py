811073027421ea1d02f2f8c35178d2759469649b
Precious Chika <43546663+pre22@users.noreply.github.com>
2022-10-30 21:25:39 +0100

    created a class

--- app/hello_word.py
+++ app/hello_word.py
@@ -2,26 +2,36 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication,QMainWindow
 import sys
 
-def clicked():
-    print("clicked")
+class MyWindow(QMainWindow):
+    def __ini__(self):
+        super(MyWindow, self).__init__()
+        self.setGeometry(200, 200, 1000, 600)
+        self.setWindowTitle("Tech With Timi!")
+        self.initUI()
+    
+    def initUI(self):
+        # Label
+        self.label = QtWidgets.QLabel(self)
+        self.label.setText("Anamo App")
+
+        # Button
+        self.b1 = QtWidgets.QPushButton(self)
+        self.b1.setText("Click me")
+        self.b1.clicked.connect(self.clicked)
+
+    def clicked(self):
+        self.label.setText("You pressedthe button")
+
 
 def window():
     app = QApplication(sys.argv)
-    win = QMainWindow()
+    win = MyWindow()
+    # win = QMainWindow()
     win.setGeometry(200, 200, 1000, 600)
     win.setWindowTitle("Tech With Timi!")
-
-    # Label
-    label = QtWidgets.QLabel(win)
-    label.setText("Anamo App")
-
-    # Button
-    b1 = QtWidgets.QPushButton(win)
-    b1.setText("Click me")
-    b1.clicked.connect(clicked)
-    
-
     win.show()
     sys.exit(app.exec_())
 
+
+# window = MyWindow()
 window()
