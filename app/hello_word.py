e26adabcc9b917de6a1712e9d965b18ca772a0c1
Precious Chika <43546663+pre22@users.noreply.github.com>
2022-10-30 15:18:16 +0100

    created button and added event to button

--- app/hello_word.py
+++ app/hello_word.py
@@ -2,14 +2,23 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication,QMainWindow
 import sys
 
+def clicked():
+    print("clicked")
+
 def window():
     app = QApplication(sys.argv)
     win = QMainWindow()
     win.setGeometry(200, 200, 1000, 600)
     win.setWindowTitle("Tech With Timi!")
 
+    # Label
     label = QtWidgets.QLabel(win)
     label.setText("Anamo App")
+
+    # Button
+    b1 = QtWidgets.QPushButton(win)
+    b1.setText("Click me")
+    b1.clicked.connect(clicked)
     
 
     win.show()
