c865e4100c038c1b091e07784bec3feaf6f725c1
Precious Chika <43546663+pre22@users.noreply.github.com>
2022-10-30 23:25:42 +0100

    changed text

--- ui_files/test.py
+++ ui_files/test.py
@@ -42,7 +42,7 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "Anamo"))
         self.pushButton.setText(_translate("MainWindow", "LOGIN"))
         self.label.setText(_translate("MainWindow", "LOGIN -SIGNUP FORM"))
 
