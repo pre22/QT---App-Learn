a6d498cb9232b3399277eafb8b90540482083089
Precious Chika <43546663+pre22@users.noreply.github.com>
2022-12-03 05:40:50 +0100

    checkbox changes

--- ui_files/layout.py
+++ ui_files/layout.py
@@ -5,40 +5,30 @@ from PyQt5.QtWidgets import (
     QDialog,
     QMainWindow
 )
-from demoRadioButton2 import Ui_Dialog
+from demoCheckBox1 import Ui_Dialog
 
 class MyForm(QDialog):
     def __init__(self):
         super().__init__()
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
-        self.ui.radioButtonMedium.toggled.connect(self.dispSelected)
-        self.ui.radioButtonLarge.toggled.connect(self.dispSelected)
-        self.ui.radioButtonXL.toggled.connect(self.dispSelected)
-        self.ui.radioButtonXXL.toggled.connect(self.dispSelected)
-        self.ui.radioButtonDebitCard.toggled.connect(self.dispSelected)
-        self.ui.radioButtonNetBanking.toggled.connect(self.dispSelected)
-        self.ui.radioButtonCashOnDelivery.toggled.connect(self.dispSelected)
+        self.ui.checkBoxCheese.stateChanged.connect(self.dispAmount)
+        self.ui.checkBoxOlives.stateChanged.connect(self.dispAmount)
+        self.ui.checkBoxSausages.stateChanged.connect(self.dispAmount)
+
         self.show()
 
-    def dispSelected(self):
-        selected1 = ""
-        selected2 = ""
-        if self.ui.radioButtonMedium.isChecked() == True:
-            selected1 = "Medium"
-        if self.ui.radioButtonLarge.isChecked() == True:
-            selected1 = "Large"
-        if self.ui.radioButtonXL.isChecked() == True:
-            selected1 = "Extra Large"
-        if self.ui.radioButtonXXL.isChecked() == True:
-            selected1 = "Extra Extra Large"
-        if self.ui.radioButtonDebitCard.isChecked() == True:
-            selected2 = "Debit/Credit Card"
-        if self.ui.radioButtonNetBanking.isChecked() == True:
-            selected2 = "Net Banking"
-        if self.ui.radioButtonCashOnDelivery.isChecked() == True:
-            selected2 = "Cash On Delivery"
-        self.ui.labelSelected.setText("Chosen shirt size is " +selected1+" and payment method as " +selected2)
+    def dispAmount(self):
+        amount = 10
+
+        if self.ui.checkBoxCheese.isChecked() == True:
+            amount = amount+1
+        if self.ui.checkBoxOlives.isChecked() == True:
+            amount = amount+1
+        if self.ui.checkBoxSausages.isChecked() == True:
+            amount = amount+2
+        
+        self.ui.labelAmount.setText("Total amount for pizza is "+str(amount))
 
     
 
