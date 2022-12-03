6b5cdbed43de36c3eee8c0e27d3b1c6db26c9713
Precious Chika <43546663+pre22@users.noreply.github.com>
2022-12-03 06:28:10 +0100

    refactor ui

--- ui_files/layout.py
+++ ui_files/layout.py
@@ -5,30 +5,41 @@ from PyQt5.QtWidgets import (
     QDialog,
     QMainWindow
 )
-from demoCheckBox1 import Ui_Dialog
+from demoCheckBox2 import Ui_Dialog
 
 class MyForm(QDialog):
     def __init__(self):
         super().__init__()
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
-        self.ui.checkBoxCheese.stateChanged.connect(self.dispAmount)
-        self.ui.checkBoxOlives.stateChanged.connect(self.dispAmount)
-        self.ui.checkBoxSausages.stateChanged.connect(self.dispAmount)
-
+        self.ui.checkBoxChocolateAlmond.stateChanged.connect(self.dispAmount)
+        self.ui.checkBoxChocolateChips.stateChanged.connect(self.dispAmount)
+        self.ui.checkBoxCookieDough.stateChanged.connect(self.dispAmount)
+        self.ui.checkBoxRockyRoad.stateChanged.connect(self.dispAmount)
+        self.ui.checkBoxCoffee.stateChanged.connect(self.dispAmount)
+        self.ui.checkBoxSoda.stateChanged.connect(self.dispAmount)
+        self.ui.checkBoxTea.stateChanged.connect(self.dispAmount)
         self.show()
 
     def dispAmount(self):
-        amount = 10
+        amount = 0
 
-        if self.ui.checkBoxCheese.isChecked() == True:
-            amount = amount+1
-        if self.ui.checkBoxOlives.isChecked() == True:
-            amount = amount+1
-        if self.ui.checkBoxSausages.isChecked() == True:
-            amount = amount+2
+        if self.ui.checkBoxChocolateAlmond.isChecked()==True:
+            amount=amount+3
+        if self.ui.checkBoxChocolateChips.isChecked()==True:
+            amount=amount+4
+        if self.ui.checkBoxCookieDough.isChecked()==True:
+            amount=amount+2
+        if self.ui.checkBoxRockyRoad.isChecked()==True:
+            amount=amount+5
+        if self.ui.checkBoxCoffee.isChecked()==True:
+            amount=amount+2
+        if self.ui.checkBoxSoda.isChecked()==True:
+            amount=amount+3
+        if self.ui.checkBoxTea.isChecked()==True:
+            amount=amount+1
         
-        self.ui.labelAmount.setText("Total amount for pizza is "+str(amount))
+        self.ui.labelAmount.setText("Total amount is $"+str(amount))
 
     
 
