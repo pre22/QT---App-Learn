c1e88fe09e544e13714466a6a90855cf89a1953e
Precious Chika <43546663+pre22@users.noreply.github.com>
2022-12-04 08:53:41 +0100

    rewrite layout

--- ui_files/layout.py
+++ ui_files/layout.py
@@ -3,22 +3,44 @@ import sys
 from PyQt5.QtWidgets import (
     QApplication,
     QDialog,
-    QMainWindow
+    QMainWindow,
+    QInputDialog,
+    QListWidgetItem
 )
-from demoListWidget3 import Ui_Dialog
+from demoListWidgetOp import Ui_Dialog
 
 class MyForm(QDialog):
     def __init__(self):
         super().__init__()
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
+        self.ui.listWidget.addItem('Ice Cream')
+        self.ui.listWidget.addItem('Soda')
+        self.ui.listWidget.addItem('Coffee')
+        self.ui.listWidget.addItem('Chocolate')
         self.ui.pushButtonAdd.clicked.connect(self.addlist)
+        self.ui.pushButtonEdit.clicked.connect(self.editlist)
+        self.ui.pushButtonDelete.clicked.connect(self.delitem)
+        self.ui.pushButtonDeleteAll.clicked.connect(self.delallitems)
         self.show()
     
     def addlist(self):
-        self.ui.listWidgetSelectedItems.addItem(self.ui.lineEditFoodItem.text())
-        self.ui.lineEditFoodItem.setText('')
-        self.ui.lineEditFoodItem.setFocus()
+        self.ui.listWidget.addItem(self.ui.lineEdit.text())
+        self.ui.lineEdit.setText('')
+        self.ui.lineEdit.setFocus()
+
+    def editlist(self):
+        row = self.ui.listWidget.currentRow()
+        newtext, ok = QInputDialog.getText(self, "Enter new text","Enter new text")
+        if ok and (len(newtext) !=0):
+            self.ui.listWidget.takeItem(self.ui.listWidget.currentRow())
+        self.ui.listWidget.insertItem(row, QListWidgetItem(newtext))
+
+    def delitem(self):
+        self.ui.listWidget.takeItem(self.ui.listWidget.currentRow())
+
+    def delallitems(self):
+        self.ui.listWidget.clear()
 
     
 
