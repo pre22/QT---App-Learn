7451da8fe5f6a4009d212a4422ad98245855c304
Precious Chika <43546663+pre22@users.noreply.github.com>
2022-12-04 17:17:33 +0100

    rewrite layout

--- ui_files/layout.py
+++ ui_files/layout.py
@@ -7,40 +7,18 @@ from PyQt5.QtWidgets import (
     QInputDialog,
     QListWidgetItem
 )
-from demoListWidgetOp import Ui_Dialog
+from demoComboBox import Ui_Dialog
 
 class MyForm(QDialog):
     def __init__(self):
         super().__init__()
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
-        self.ui.listWidget.addItem('Ice Cream')
-        self.ui.listWidget.addItem('Soda')
-        self.ui.listWidget.addItem('Coffee')
-        self.ui.listWidget.addItem('Chocolate')
-        self.ui.pushButtonAdd.clicked.connect(self.addlist)
-        self.ui.pushButtonEdit.clicked.connect(self.editlist)
-        self.ui.pushButtonDelete.clicked.connect(self.delitem)
-        self.ui.pushButtonDeleteAll.clicked.connect(self.delallitems)
+        self.ui.comboBoxAccountType.currentIndexChanged.connect(self.dispAccountType)
         self.show()
     
-    def addlist(self):
-        self.ui.listWidget.addItem(self.ui.lineEdit.text())
-        self.ui.lineEdit.setText('')
-        self.ui.lineEdit.setFocus()
-
-    def editlist(self):
-        row = self.ui.listWidget.currentRow()
-        newtext, ok = QInputDialog.getText(self, "Enter new text","Enter new text")
-        if ok and (len(newtext) !=0):
-            self.ui.listWidget.takeItem(self.ui.listWidget.currentRow())
-        self.ui.listWidget.insertItem(row, QListWidgetItem(newtext))
-
-    def delitem(self):
-        self.ui.listWidget.takeItem(self.ui.listWidget.currentRow())
-
-    def delallitems(self):
-        self.ui.listWidget.clear()
+    def dispAccountType(self):
+        self.ui.labelAccountType.setText("You have selected "+self.ui.comboBoxAccountType.itemText(self.ui.comboBoxAccountType.currentIndex()))
 
     
 
