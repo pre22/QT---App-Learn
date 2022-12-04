7ba69d38f80039ea0df114878e71e85461ccf091
Precious Chika <43546663+pre22@users.noreply.github.com>
2022-12-04 07:18:43 +0100

    rewrite file

--- ui_files/layout.py
+++ ui_files/layout.py
@@ -5,37 +5,18 @@ from PyQt5.QtWidgets import (
     QDialog,
     QMainWindow
 )
-from demoSpinner import Ui_Dialog
+from demoListWidget import Ui_Dialog
 
 class MyForm(QDialog):
     def __init__(self):
         super().__init__()
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
-        self.ui.spinBoxBookQty.editingFinished.connect(self.result1)
-        self.ui.doubleSpinBoxSugarWeight.editingFinished.connect(self.result2)
+        self.ui.listWidgetDiagnosis.itemClicked.connect(self.dispSelectedTest)
         self.show()
-
-    def result1(self):
-        if len(self.ui.lineEditBookPrice.text())!=0:
-            bookPrice=int(self.ui.lineEditBookPrice.text())
-        else:
-            bookPrice=0
-        totalBookAmount=self.ui.spinBoxBookQty.value() * bookPrice
-        self.ui.lineEditBookAmount.setText(str(totalBookAmount))
     
-    def result2(self):
-        if len(self.ui.lineEditSugarPrice.text())!=0:
-            sugarPrice=float(self.ui.lineEditSugarPrice.text())
-        else:
-            sugarPrice=0
-        totalSugarAmount=self.ui.doubleSpinBoxSugarWeight.value() * sugarPrice
-        self.ui.lineEditSugarAmount.setText(str(round(totalSugarAmount,2)))
-        
-        totalBookAmount=int(self.ui.lineEditBookAmount.text())
-        totalAmount=totalBookAmount+totalSugarAmount
-        
-        self.ui.labelTotalAmount.setText(str(round(totalAmount,2)))
+    def dispSelectedTest(self):
+        self.ui.labelTest.setText("You have selected "+self.ui.listWidgetDiagnosis.currentItem().text())
 
 
 
